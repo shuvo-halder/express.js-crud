793e1687461556a5d571891329b22405706a81ce
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-08 18:00:38 +0600

    adding put route and getting function

--- index.js
+++ index.js
@@ -40,11 +40,21 @@ app.post("/books", async (req,res) => {
 })
 
 // delete route and function
-
 app.delete("/books/:id", async (req,res) => {
   try {
     const {id} = req.params;
     res.status(200).json({message: `delete id : ${id} `})
+  } catch (error) {
+    res.json({error: error.message})
+  }
+})
+
+// route update
+
+app.put("/books", async (req,res) => {
+  try {
+    const {name, description} = req.body;
+    res.status(201).json({message: `books was created ${name}, ${description}`})
   } catch (error) {
     res.json({error: error.message})
   }
