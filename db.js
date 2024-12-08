81fcdf50bffdaa2327331fd378e313b6f7865fd7
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-08 21:37:18 +0600

    postgre database connection #db

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const { Pool } = require("pg");
 
-
 const pool = new Pool({
     host: 'localhost',
     user: 'shuvo',
-    port:5432,
-    database:'bookDB',
+    port: 5432,
+    database: 'bookDB',
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
-  })
+})
 
 module.exports = pool;
