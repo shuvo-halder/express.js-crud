140f9e6d4a7c7af63efe4bc6dc40f8a2b028bbfe
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-08 21:35:00 +0600

    adding pg and database connection

---
+++ db.js
@@ -0,0 +1,13 @@
+const { Pool } = require("pg");
+
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'shuvo',
+    port:5432,
+    database:'bookDB',
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  })
+
+module.exports = pool;
